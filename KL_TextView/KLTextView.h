745959ffef6281671cbd60b3bed91d9a069219d1
赵凯乐 <zhaokaile@zhaokailes-MacBook-Pro.local>
2018-03-23 12:19:28 +0800

    first commit

---
+++ KL_TextView/KLTextView.h
@@ -0,0 +1,13 @@
+//
+//  KLTextView.h
+//  KL_TextView
+//
+//  Created by 赵凯乐 on 2018/3/22.
+//  Copyright © 2018年 赵凯乐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KLTextView : UITextView
+
+@end
---
+++ KL_TextView/KLTextView.m
@@ -0,0 +1,21 @@
+//
+//  KLTextView.m
+//  KL_TextView
+//
+//  Created by 赵凯乐 on 2018/3/22.
+//  Copyright © 2018年 赵凯乐. All rights reserved.
+//
+
+#import "KLTextView.h"
+
+@implementation KLTextView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
